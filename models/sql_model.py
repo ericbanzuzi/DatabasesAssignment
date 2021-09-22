59c0d5972370c7fc27a9155b9cbb7413bf41b280
ericbanzuzi <eric.banzuzi@gmail.com>
2021-09-22 10:44:45 +0200

    flask

--- models/sql_model.py
+++ models/sql_model.py
@@ -1,92 +1,91 @@
-from sqlalchemy import create_engine
+from pathlib import Path
+from app import app
+from flask_sqlalchemy import SQLAlchemy
 
-engine = create_engine('mysql://ericbanzuzi:mysql00eb@localhost/dbproject', echo=True)
+Path("database").mkdir(parents=True, exist_ok=True)  # needed?
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://ericbanzuzi:mysql00eb@localhost/dbproject'
+db = SQLAlchemy(app)
 
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
-from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, DECIMAL
 
-Session = sessionmaker(bind=engine)
-session = Session()
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    firstname = db.Column(db.String(80), nullable=False)
+    lastname = db.Column(db.String(80), nullable=False)
+    phone_number = db.Column(db.String(20), nullable=False)
 
-Base = declarative_base()
-
-class Customer(Base):
-    __tablename__ = 'customer'
-
-    id = Column(Integer, primary_key=True)
-    firstname = Column(String(80), nullable=False)
-    lastname = Column(String(80), nullable=False)
-    phone_number = Column(String(20), nullable=False)
     # address foreign key?
-    # TODO: add relationships
-    def __repr__(self):
-        return f'Customer({id}, {firstname}, {lastname}, {phone_number})'
-
-class Address(Base):
-    __tablename__ = 'address'
-    id = Column(Integer, primary_key=True)
-    street = Column(String(80), nullable=False)
-    house_number = Column(String(5), nullable=False)
-    area = Column(String(80), nullable=False) # or city, how do we define area???
-    postcode = Column(String(10), nullable=False)
 
     # TODO: add relationships
-
     def __repr__(self):
-        return f'Address({id}, {street}, {housenumber}, {area}, {postcode})'
+        return f'Customer({id}, {firstname}, {lastname}, {phone_number}, {address})'
 
-class Order(Base):
-    __tablename__ = 'order'
 
-    id = Column(Integer, primary_key=True)
-    customer_id = Column(Integer, ForeignKey('customer.id'))
-    order_date = Column(String(80), nullable=False)
-    # discount code?
+class Address(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    street = db.Column(db.String(80), nullable=False)
+    house_number = db.Column(db.String(5), nullable=False)
+    city = db.Column(db.String(80), nullable=False)
+    postcode = db.Column(db.String(10), nullable=False)
+
+    # TODO: add relationships
+    def __repr__(self):
+        return f'Address({id}, {street}, {housenumber}, {city}, {postcode})'
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    customer_id = db.Column(db.Integer, ForeignKey('customer.id'))
+    order_date = db.Column(db.DateTime, nullable=False)
+    discount_code = db.Column(db.Boolean, nullable=False)
 
     # TODO: add relationships
     customer = relationship("Customer", back_populates="order")
+
     def __repr__(self):
-        return f'Order({id}, )'
+        return f'Order({id}, {customer_id}, {order_date}, {discount_code})'
 
-class Orderline(Base):
-    __tablename__ = 'orderline'
 
-    id = Column(Integer, primary_key=True)
-    order_id = Column(Integer, ForeignKey('order.id'))
-    pizza_id = Column(Integer, ForeignKey('pizza.id'))
-    quantity = Column(Integer, nullable=False)
+class Orderline(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    quantity = db.Column(db.Integer, nullable=False)
+
     # drink and desert linked nicely
 
     # TODO: add relationships
 
     def __repr__(self):
-        return f'Orderline({id}, )'
+        return f'Orderline({id}, {order_id}, {pizza_id}, {quantity} )'
 
 
-class Pizza(Base):
-    __tablename__ = 'pizza'
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    price = Column(DECIMAL(6,2), nullable=False)
-    vegeterian = Column(Boolean, nullable=False)
+class Pizza(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.DECIMAL(6, 2), nullable=False)  # data anomaly??
+    vegeterian = db.Column(db.Boolean, nullable=False)
 
     # TODO: add relationships
 
     def __repr__(self):
-        return f'Pizza({id}, )'
+        return f'Pizza({id}, {name}, {vegeterian})'
 
-class Topping(Base):
-    __tablename__ = 'topping'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String(80), nullable=False)
-    price = Column(DECIMAL(6,2), nullable=False)
-    vegeterian = Column(Boolean, nullable=False)
+class Topping(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.DECIMAL(6, 2), nullable=False)
+    vegeterian = db.Column(db.Boolean, nullable=False)  # ??
 
     # TODO: add relationships
 
     def __repr__(self):
-        return f'Topping({id}, )'
+        return f'Topping({id}, {name}, {price}, {vegeterian} )'
 
-Base.metadata.create_all(engine)
+
+class PizzaToppings(db.Model):
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    topping_id = db.Column(db.Integer, db.ForeignKey('topping.id'))
+
+
+db.create_all()

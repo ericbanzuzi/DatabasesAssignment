76739382763a50ff5b422cd892626d3634b53c97
ericbanzuzi <eric.banzuzi@gmail.com>
2021-09-24 20:44:24 +0200

    model, need to add the actual data in tables

--- models/sql_model.py
+++ models/sql_model.py
@@ -2,8 +2,7 @@ from pathlib import Path
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
-Path("database").mkdir(parents=True, exist_ok=True)  # needed?
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://ericbanzuzi:mysql00eb@localhost/dbproject'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://ericbanzuzi:mysql00eb@localhost/db_project'
 db = SQLAlchemy(app)
 
 
@@ -12,10 +11,11 @@ class Customer(db.Model):
     firstname = db.Column(db.String(80), nullable=False)
     lastname = db.Column(db.String(80), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
+    address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
 
-    # address foreign key?
+    # relationships
+    address = db.relationship('Address', back_populates='customer')
 
-    # TODO: add relationships
     def __repr__(self):
         return f'Customer({id}, {firstname}, {lastname}, {phone_number}, {address})'
 
@@ -27,19 +27,19 @@ class Address(db.Model):
     city = db.Column(db.String(80), nullable=False)
     postcode = db.Column(db.String(10), nullable=False)
 
-    # TODO: add relationships
     def __repr__(self):
         return f'Address({id}, {street}, {housenumber}, {city}, {postcode})'
 
 
 class Order(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.Integer, ForeignKey('customer.id'))
-    order_date = db.Column(db.DateTime, nullable=False)
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'))
+    order_datetime = db.Column(db.DateTime, nullable=False)
     discount_code = db.Column(db.Boolean, nullable=False)
 
-    # TODO: add relationships
-    customer = relationship("Customer", back_populates="order")
+    # relationships
+    customer = db.relationship('Customer', back_populates='order')
+    delivery = db.relationship('DeliveryPerson', secondary='delivery', back_populates='delivery')
 
     def __repr__(self):
         return f'Order({id}, {customer_id}, {order_date}, {discount_code})'
@@ -49,43 +49,88 @@ class Orderline(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     order_id = db.Column(db.Integer, db.ForeignKey('order.id'))
     pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
+    drink_id = db.Column(db.Integer, db.ForeignKey('drink.id'))
+    desert_id = db.Column(db.Integer, db.ForeignKey('desert.id'))
     quantity = db.Column(db.Integer, nullable=False)
 
-    # drink and desert linked nicely
-
-    # TODO: add relationships
+    # relationships
+    order = db.relationship('Order', back_populates='orderline')
+    pizza = db.relationship('Pizza', back_populates='orderline')
+    drink = db.relationship('Drink', back_populates='orderline')
+    desert = db.relationship('Desert', back_populates='orderline')
 
     def __repr__(self):
-        return f'Orderline({id}, {order_id}, {pizza_id}, {quantity} )'
+        return f'Orderline({id}, {order_id}, {pizza_id}, {drink_id}, {desert_id}, {quantity})'
 
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    price = db.Column(db.DECIMAL(6, 2), nullable=False)  # data anomaly??
-    vegeterian = db.Column(db.Boolean, nullable=False)
+    vegeterian = db.Column(db.Boolean, nullable=False) # denormalization?
 
-    # TODO: add relationships
+    # relationships
+    toppings = db.relationship('Topping', secondary='pizza_toppings', back_populates='pizza')
 
     def __repr__(self):
         return f'Pizza({id}, {name}, {vegeterian})'
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.DECIMAL(6, 2), nullable=False)
+
+    def __repr__(self):
+        return f'Drink({id}, {name}, {price})'
+
+
+class Desert(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), nullable=False)
+    price = db.Column(db.DECIMAL(6, 2), nullable=False)
+
+    def __repr__(self):
+        return f'Desert({id}, {name}, {price})'
 
 
 class Topping(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.DECIMAL(6, 2), nullable=False)
-    vegeterian = db.Column(db.Boolean, nullable=False)  # ??
+    vegeterian = db.Column(db.Boolean, nullable=False)
 
-    # TODO: add relationships
+    # relationships
+    pizza = db.relationship('Pizza', secondary='pizza_toppings', back_populates='topping')
 
     def __repr__(self):
-        return f'Topping({id}, {name}, {price}, {vegeterian} )'
+        return f'Topping({id}, {name}, {price}, {vegeterian})'
 
 
 class PizzaToppings(db.Model):
-    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'))
-    topping_id = db.Column(db.Integer, db.ForeignKey('topping.id'))
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizza.id'), primary_key=True)
+    topping_id = db.Column(db.Integer, db.ForeignKey('topping.id'), primary_key=True)
 
+    def __repr__(self):
+        return f'Pizza_toppings({pizza_id}, {topping_id})'
+
+
+class DeliveryPerson(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    area_code = db.Column(db.String(10), nullable=False)
+
+    # relationships
+    pizza = db.relationship('Order', secondary='delivery', back_populates='order')
+
+    def __repr__(self):
+        return f'Delivery_person({id}, {area_code})'
+
+
+class Delivery(db.Model):
+    delivery_person_id = db.Column(db.Integer, db.ForeignKey('delivery_person.id'), primary_key=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), primary_key=True)
+    estimated_time = db.Column(db.DateTime, nullable=False)
+
+    def __repr__(self):
+        return f'Delivery({delivery_person_id}, {order_id}, {estimated_time})'
 
 db.create_all()

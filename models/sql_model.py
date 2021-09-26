a8af52c68178ceb715eb378acfff7b441787f052
ericbanzuzi <eric.banzuzi@gmail.com>
2021-09-26 09:41:53 +0200

    model update

--- models/sql_model.py
+++ models/sql_model.py
@@ -1,4 +1,3 @@
-from pathlib import Path
 from app import app
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,11 +12,12 @@ class Customer(db.Model):
     phone_number = db.Column(db.String(20), nullable=False)
     address_id = db.Column(db.Integer, db.ForeignKey('address.id'), nullable=False)
 
-    # relationships
+    # relationships, many to one
     address = db.relationship('Address', back_populates='customer')
+    order = db.relationship('Order', back_populates='customer')
 
     def __repr__(self):
-        return f'Customer({id}, {firstname}, {lastname}, {phone_number}, {address})'
+        return f'Customer({id}, {self.firstname}, {self.lastname}, {self.phone_number}, {self.address})'
 
 
 class Address(db.Model):
@@ -27,8 +27,10 @@ class Address(db.Model):
     city = db.Column(db.String(80), nullable=False)
     postcode = db.Column(db.String(10), nullable=False)
 
+    customer = db.relationship('Customer', order_by=Customer.id, back_populates="address")
+
     def __repr__(self):
-        return f'Address({id}, {street}, {housenumber}, {city}, {postcode})'
+        return f'Address({id}, {self.street}, {self.housenumber}, {self.city}, {self.postcode})'
 
 
 class Order(db.Model):
@@ -38,11 +40,11 @@ class Order(db.Model):
     discount_code = db.Column(db.Boolean, nullable=False)
 
     # relationships
-    customer = db.relationship('Customer', back_populates='order')
-    delivery = db.relationship('DeliveryPerson', secondary='delivery', back_populates='delivery')
+    customer = db.relationship('Customer', back_populates='order', uselist=False) # many to one
+    delivery = db.relationship('DeliveryPerson', secondary='delivery', back_populates='delivery', uselist=False) # one to one
 
     def __repr__(self):
-        return f'Order({id}, {customer_id}, {order_date}, {discount_code})'
+        return f'Order({id}, {self.customer_id}, {self.order_date}, {self.discount_code})'
 
 
 class Orderline(db.Model):
@@ -53,26 +55,26 @@ class Orderline(db.Model):
     desert_id = db.Column(db.Integer, db.ForeignKey('desert.id'))
     quantity = db.Column(db.Integer, nullable=False)
 
-    # relationships
-    order = db.relationship('Order', back_populates='orderline')
-    pizza = db.relationship('Pizza', back_populates='orderline')
-    drink = db.relationship('Drink', back_populates='orderline')
-    desert = db.relationship('Desert', back_populates='orderline')
+    # relationships, many to many
+    order = db.relationship('Order')
+    pizza = db.relationship('Pizza')
+    drink = db.relationship('Drink')
+    desert = db.relationship('Desert')
 
     def __repr__(self):
-        return f'Orderline({id}, {order_id}, {pizza_id}, {drink_id}, {desert_id}, {quantity})'
+        return f'Orderline({id}, {self.order_id}, {self.pizza_id}, {self.drink_id}, {self.desert_id}, {self.quantity})'
 
 
 class Pizza(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    vegeterian = db.Column(db.Boolean, nullable=False) # denormalization?
+    # vegetarian = db.Column(db.Boolean, nullable=False) # denormalization?
 
     # relationships
     toppings = db.relationship('Topping', secondary='pizza_toppings', back_populates='pizza')
 
     def __repr__(self):
-        return f'Pizza({id}, {name}, {vegeterian})'
+        return f'Pizza({id}, {self.name})'
 
 
 class Drink(db.Model):
@@ -81,7 +83,7 @@ class Drink(db.Model):
     price = db.Column(db.DECIMAL(6, 2), nullable=False)
 
     def __repr__(self):
-        return f'Drink({id}, {name}, {price})'
+        return f'Drink({id}, {self.name}, {self.price})'
 
 
 class Desert(db.Model):
@@ -90,20 +92,20 @@ class Desert(db.Model):
     price = db.Column(db.DECIMAL(6, 2), nullable=False)
 
     def __repr__(self):
-        return f'Desert({id}, {name}, {price})'
+        return f'Desert({id}, {self.name}, {self.price})'
 
 
 class Topping(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     price = db.Column(db.DECIMAL(6, 2), nullable=False)
-    vegeterian = db.Column(db.Boolean, nullable=False)
+    vegetarian = db.Column(db.Boolean, nullable=False)
 
     # relationships
-    pizza = db.relationship('Pizza', secondary='pizza_toppings', back_populates='topping')
+    pizza = db.relationship('Pizza', secondary='pizza_toppings', back_populates='toppings')
 
     def __repr__(self):
-        return f'Topping({id}, {name}, {price}, {vegeterian})'
+        return f'Topping({id}, {self.name}, {self.price}, {self.vegetarian})'
 
 
 class PizzaToppings(db.Model):
@@ -111,18 +113,18 @@ class PizzaToppings(db.Model):
     topping_id = db.Column(db.Integer, db.ForeignKey('topping.id'), primary_key=True)
 
     def __repr__(self):
-        return f'Pizza_toppings({pizza_id}, {topping_id})'
+        return f'Pizza_toppings({self.pizza_id}, {self.topping_id})'
 
 
 class DeliveryPerson(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     area_code = db.Column(db.String(10), nullable=False)
 
-    # relationships
-    pizza = db.relationship('Order', secondary='delivery', back_populates='order')
+    # relationships, many to one
+    delivery = db.relationship('Order', secondary='delivery', back_populates='delivery')
 
     def __repr__(self):
-        return f'Delivery_person({id}, {area_code})'
+        return f'Delivery_person({id}, {self.area_code})'
 
 
 class Delivery(db.Model):
@@ -131,6 +133,100 @@ class Delivery(db.Model):
     estimated_time = db.Column(db.DateTime, nullable=False)
 
     def __repr__(self):
-        return f'Delivery({delivery_person_id}, {order_id}, {estimated_time})'
+        return f'Delivery({self.delivery_person_id}, {self.order_id}, {self.stimated_time})'
+
+
+def create_menu():
+    mozzarella = Topping(name='mozzarella', price=1.59, vegetarian=True)
+    tomato_sauce = Topping(name='tomato sauce', price=0.99, vegetarian=True)
+    oregano = Topping(name='oregano', price=0.49, vegetarian=True)
+    ham = Topping(name='ham', price=2.49, vegetarian=False)
+    pineapple = Topping(name='pineapple', price=1.20, vegetarian=True)
+    mushroom = Topping(name='mushroom', price=1.00, vegetarian=True)
+    onion = Topping(name='onion', price=0.39, vegetarian=True)
+    pepper = Topping(name='pepper', price=1.20, vegetarian=True)
+    tomato = Topping(name='tomato', price=0.49, vegetarian=True)
+    spicy_salami = Topping(name="spicy salami", price=2.99, vegetarian=False)
+    pepperoni = Topping(name='pepperoni', price=2.49, vegetarian=False)
+    egg = Topping(name='egg', price = 1.49, vegetarian=True)
+    chicken = Topping(name='chicken', price = 2.49, vegetarian=False)
+    eggplant = Topping(name='eggplant',price = 1.49)
+
+    pizza = Pizza(name='Margherita')
+    pizza.toppings = [
+        tomato_sauce, mozzarella, oregano, Topping(name='garlic oil', price=0.49, vegetarian=True)
+    ]
+
+    pizza2 = Pizza(name='Hawaii')
+    pizza2.toppings = [
+        tomato_sauce, mozzarella,  ham, pineapple
+    ]
+
+    pizza3 = Pizza(name='Veggie')
+    pizza3.toppings = [
+        tomato_sauce, mozzarella, onion, tomato, mushroom, pepper
+    ]
+    pizza4 = Pizza(name='Forest')
+    pizza4.toppings = [
+        tomato_sauce, mozzarella, mushroom, ham
+    ]
+    pizza5 = Pizza(name='Diavolo')
+    pizza5.toppings = [
+        tomato_sauce, mozzarella, spicy_salami, onion, pepper
+    ]
+    pizza6 = Pizza(name='Pepperoni')
+    pizza6.toppings = [
+        tomato_sauce, mozzarella, pepperoni
+    ]
+    pizza7 = Pizza(name='Peppel Pizza')
+    pizza7.toppings = [
+        tomato_sauce, mozzarella, pepperoni, pepper, chicken
+    ]
+    pizza8 = Pizza(name='Romano')
+    pizza8.toppings = [
+        tomato_sauce, mozzarella, oregano, spicy_salami, egg, tomato
+    ]
+    pizza9 = Pizza(name='Veggie Deluxe')
+    pizza9.toppings = [
+        tomato_sauce, mozzarella, onion, tomato, mushroom, pepper, oregano, eggplant
+    ]
+    pizza10 = Pizza(name='Meatlovers')
+    pizza10.toppings = [
+        tomato_sauce, pepperoni, spicy_salami, ham, chicken, onion
+    ]
+
+    # drinks
+    coke = Drink(name='Coca-Cola', price=1.99)
+    fanta = Drink(name='Fanta', price=1.99)
+
+    # deserts
+    cheese_cake = Desert(name='Cheese cake', price=3.50)
+    ice_cream = Desert(name='Ice cream', price=2.50)
+
+    db.session.add_all([
+        pizza, pizza2, pizza3, pizza4, pizza5, pizza6, pizza7, pizza8, pizza9, pizza10
+        coke, fanta, cheese_cake, ice_cream
+    ])
+    db.session.commit()
+
+
+def create_delivery_persons():
+    db.session.add_all([
+        DeliveryPerson(area_code='60'),
+        DeliveryPerson(area_code='60'),
+        DeliveryPerson(area_code='61'),
+        DeliveryPerson(area_code='61'),
+        DeliveryPerson(area_code='62'),
+        DeliveryPerson(area_code='62'),
+        DeliveryPerson(area_code='63'),
+        DeliveryPerson(area_code='63'),
+        DeliveryPerson(area_code='64'),
+        DeliveryPerson(area_code='64')
+    ])
+    db.session.commit()
+
 
 db.create_all()
+create_menu()
+create_delivery_persons()
+
